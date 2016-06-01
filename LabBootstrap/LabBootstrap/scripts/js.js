0112938f1b602adfed506abf4359aa50eb711014
CashmereBob <weidmar@gmail.com>
2016-06-01 13:25:30 +0200

    game of maldives

--- LabBootstrap/LabBootstrap/scripts/js.js
+++ LabBootstrap/LabBootstrap/scripts/js.js
@@ -10,5 +10,30 @@
         }, 900, 'swing', function () {
             window.location.hash = target;
         });
+
+
     });
+
+    var height = document.body.clientHeight;
+    var width = document.body.clientWidth;
+    var htmlCanvas = document.getElementById('canvas');
+
+    htmlCanvas.width = window.innerWidth;
+    htmlCanvas.height = window.innerHeight;
+
+    var img = new Image();
+    img.onload = function () {
+        var ctx = document.getElementById('canvas').getContext('2d');
+        ctx.drawImage(img, 0, 0, img.width, img.height,    // source rectangle
+                   0, 0, canvas.width, canvas.height); // destination rectangle
+    }
+    img.src = '../images/aquarium.png';
+
+    $("#diver").css("animation-duration", "15s")
+    $("#divercontrol").on('input', function () {
+        var value = $(this).val();
+        value = 31 - parseInt(value);
+        $("#diver").css("animation-duration", value + "s");
+    });
+
 });
